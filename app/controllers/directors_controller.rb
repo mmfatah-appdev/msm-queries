d04cc00c4a0cd2c49465adce67e41cb6e52c8325
mmfatah <muufo.24@gmail.com>
2023-03-28 18:39:56 +0000

    quarter completed

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,8 +1,13 @@
 class DirectorsController < ApplicationController
 
   def index
+    @list_of_directors = Director.all
     
     render({ :template => "director_templates/index.html.erb"})
   end
-  
+
+  def wisest
+    @oldest = Director.where.not({ :dob => nil }).order({ :dob => :asc }).at(0)
+    render({ :template => "director_templates/eldest.html.erb"})
+  end
   end
